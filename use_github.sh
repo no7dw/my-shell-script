a0504d63981a5cb2e057eb3be90c805f8eae530d
no7dw <no7david@gmail.com>
2015-03-24 17:14:27 +0800

    add change config shell script

---
+++ use_github.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mv ~/.ssh ~/.ssh_gitlab
+mv ~/.ssh_github ~/.ssh
+echo 'using github now'
---
+++ use_gitlab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mv ~/.ssh ~/.ssh_github
+mv ~/.ssh_gitlab ~/.ssh
+echo 'using gitlab now'
