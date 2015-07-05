25a93ee26d29dec414d85c7c1a762662feb93059
no7david <no7david@gmail.com>
2015-07-05 23:00:31 +0800

    test before modify

--- use_github.sh
+++ use_github.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+folder="/Users/mac/.ssh_gitlab"
+if [  -d "$folder" ];then
+    echo $folder' not exists'
+    echo 'perhaps you are already using github config'
+    exit 1
+fi
 mv ~/.ssh ~/.ssh_gitlab
 mv ~/.ssh_github ~/.ssh
+git config --global user.name no7david
+git config --global user.email no7david@gmail.com
 echo 'using github now'
--- use_gitlab.sh
+++ use_gitlab.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+folder="/Users/mac/.ssh_github"
+if [  -d "$folder" ];then
+    echo $folder' not exists'
+    echo 'perhaps you are already using gitlab config'
+    echo `git config --list`
+    exit 1
+fi
 mv ~/.ssh ~/.ssh_github
 mv ~/.ssh_gitlab ~/.ssh
+git config --global user.name Wade
+git config --global user.email wdeng@kalengo.com
+
 echo 'using gitlab now'
